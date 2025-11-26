9cce106bb7233e8cd076bfa424b103c69c90fd79
Home Desktop <auggie.sabana@gmail.com>
2025-11-26 10:25:22 -0500

    Starting to give our player movment

--- Midnight_Feast/Assets/Scripts/Player_Movment.cs
+++ Midnight_Feast/Assets/Scripts/Player_Movment.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class Player_Movment : MonoBehaviour
 {
+
+    private float xMovement;
+    private float yMovement;
+    private float speed; 
+    bool isFacingLeft;
+    bool isFacingRight;
+    bool isFacingUp;
+    bool isfacingDown;
+
+    [SerializedField] private RigidBody2D rb ;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +22,12 @@ public class Player_Movment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        xMove = Input.getAxisRaw("xMovement"); 
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(xMovement * speed, rb.velocity.y); 
     }
 }
