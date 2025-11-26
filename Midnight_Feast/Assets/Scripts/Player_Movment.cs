1ea7e938295435625a84aab2ee1d40b1aac2e6e7
Home Desktop <auggie.sabana@gmail.com>
2025-11-26 10:38:55 -0500

    Tutorial Watching code; Update later for us

--- Midnight_Feast/Assets/Scripts/Player_Movment.cs
+++ Midnight_Feast/Assets/Scripts/Player_Movment.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.inputSystem;
 
 public class Player_Movment : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+[SerializeField] private float moveSpeed = 5f;
 
-    // Update is called once per frame
-    void Update()
+private Vector2 movement ;
+private Rigidbody2D _rb;
+private Animator animator;
+
+private const string _xAxis = "Horizontal";
+private const string _yAxis = "Vertical";
+private const string _LastXAxis = "LastHorizontal";
+private const string _LastYAxis = "LastVertical";
+private void update()
     {
-        
+        movement.set(InputManager.movement.x, InputManager.movement.y);
+
+        _rb.velocity = movement + moveSpeed;
+
+        animator.SetFloat()
     }
 }
