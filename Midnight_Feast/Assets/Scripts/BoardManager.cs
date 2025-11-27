ebb528407d06d3ae36f0f1c4440112b3d26a56e6
brownk75 <brownk75@mailbox.winthrop.edu>
2025-11-27 10:10:39 -0500

    Just added functionality for being able to pass through certain tiles or not

--- Midnight_Feast/Assets/Scripts/BoardManager.cs
+++ Midnight_Feast/Assets/Scripts/BoardManager.cs
@@ -3,7 +3,12 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
-    
+    public class CellData
+    {
+        public bool Passable; // Decides if tiles can be passed through or not
+    }
+
+    private CellData[,] m_BoardData;
     private Tilemap m_Tilemap;
 
     public int Width;
@@ -16,19 +21,24 @@ public class BoardManager : MonoBehaviour
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
 
+        m_BoardData = new CellData[Width, Height];
+
         for (int y = 0; y < Height; y++)
         {
             for(int x = 0; x < Width; x++)
             {
                 Tile tile;
+                m_BoardData[x,y] = new CellData();
 
                 if(x == 0 || y == 0 || x == Width-1 || y == Height - 1)
                 {
                     tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    m_BoardData[x,y].Passable = false;
                 }
                 else
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    m_BoardData[x,y].Passable = true;
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
