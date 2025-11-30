9a2471edaad08fffc57f25425be18160b614ed00
Morriss19 <Morriss19@mailbox.winthrop.edu>
2025-11-30 18:17:00 -0500

    can now generate food on the board, still needs collection mechanic though

--- Midnight_Feast/Assets/Scripts/BoardManager.cs
+++ Midnight_Feast/Assets/Scripts/BoardManager.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
 public class BoardManager : MonoBehaviour
 {
     public class CellData
     {
         public bool Passable; // Decides if tiles can be passed through or not
+        public GameObject ContainedObject;
     }
 
     private CellData[,] m_BoardData;
+    private List<Vector2Int> m_EmptyCellsList;
     private Tilemap m_Tilemap;
     private Grid m_Grid;
 
@@ -17,6 +20,9 @@ public class BoardManager : MonoBehaviour
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
     public PlayerController Player;
+    public GameObject FoodPrefab;
+    public int foodCountMin;
+    public int foodCountMax;
     
     public Vector3 CellToWorld(Vector2Int cellIndex)
     {
@@ -33,11 +39,31 @@ public class BoardManager : MonoBehaviour
     // Check if cell is passable
     return m_BoardData[cellIndex.x, cellIndex.y].Passable;
 }
+
+    void GenerateFood()
+    {
+        int foodCount = Random.Range(foodCountMin, foodCountMax);
+        for (int i = 0; i < foodCount; ++i)
+        {
+            int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
+            Vector2Int coord = m_EmptyCellsList[randomIndex];
+            
+            m_EmptyCellsList.RemoveAt(randomIndex);
+            CellData data = m_BoardData[coord.x, coord.y];
+            GameObject newFood = Instantiate(FoodPrefab);
+            newFood.transform.position = CellToWorld(coord);
+            data.ContainedObject = newFood;
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Init()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
         m_Grid = GetComponentInChildren<Grid>();
+
+        // initializes the list
+        m_EmptyCellsList = new List<Vector2Int>();
 
         m_BoardData = new CellData[Width, Height];
 
@@ -57,11 +83,18 @@ public class BoardManager : MonoBehaviour
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
                     m_BoardData[x,y].Passable = true;
+
+                    //passable empty cell
+                    m_EmptyCellsList.Add(new Vector2Int(x, y));
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+
+        // remove the starting point of player
+        m_EmptyCellsList.Remove(new Vector2Int(1, 1));
+        GenerateFood();
     }
 
     
