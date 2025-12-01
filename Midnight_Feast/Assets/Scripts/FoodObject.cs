f0a144ef8609e9690f93b4f99ba6ac9841d6e348
Home Desktop <auggie.sabana@gmail.com>
2025-11-30 22:22:17 -0500

    Continue Food stuffs

--- Midnight_Feast/Assets/Scripts/FoodObject.cs
+++ Midnight_Feast/Assets/Scripts/FoodObject.cs
@@ -2,22 +2,16 @@ using UnityEngine;
 
 public class FoodObject : CellObject
 {
+    [SerializedFeield] private int foodValue = 10;
     public override void PlayerEntered()
     {
-        Destroy(gameObject);
+        Debug.log("Food Collected");
 
-        Debug.Log("Food increased");
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ChangeFood(foodValue);
+        }
+        Destroy(/*Let me get the food object*/); 
     }
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
